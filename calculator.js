7c6f1d3d247b6a24ece22d31984c97db7a60a578
Nikunj Patel <nikunj.patel@hotmail.com>
2020-08-13 16:30:31 +0530

    added new changes

--- calculator.js
+++ calculator.js
@@ -1,8 +1,3 @@
-var a = document.getElementsByTagName("input");
-var b = document.getElementsByTagName("button");
-for(items of button){
-    items.addEventListner('click', (e)=>{
-        buttonText = e.target.innerText;
-        console.log(buttonText);
-    })
+function calc(valx){
+   var a = document.getElementById('input').value += valx;
 }
